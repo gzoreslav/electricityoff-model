d3248bd2743e59283ce3188a1789ffeec9e2622c
Zoreslav Goral <zgoral_accentiko@genie.health>
2024-06-06 14:55:11 +0300

    fix ts

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-import { OnOffEnum } from "./models/onOffEnum";
-export { OnOffEnum };
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OnOffEnum = void 0;
+const onOffEnum_1 = require("./models/onOffEnum");
+Object.defineProperty(exports, "OnOffEnum", { enumerable: true, get: function () { return onOffEnum_1.OnOffEnum; } });
 //# sourceMappingURL=index.js.map
--- models/onOffEnum.js
+++ models/onOffEnum.js
@@ -1,7 +1,10 @@
-export var OnOffEnum;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OnOffEnum = void 0;
+var OnOffEnum;
 (function (OnOffEnum) {
     OnOffEnum["on"] = "on";
     OnOffEnum["off"] = "off";
     OnOffEnum["maybe"] = "maybe";
-})(OnOffEnum || (OnOffEnum = {}));
+})(OnOffEnum || (exports.OnOffEnum = OnOffEnum = {}));
 //# sourceMappingURL=onOffEnum.js.map
